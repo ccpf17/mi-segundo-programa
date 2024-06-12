5ad3f8d8bda1cc4050f3bfc5e67e1379821bbd30
ccpf17 <ccpf17@educastillalamancha.es>
2024-06-12 11:48:49 +0200

    Jugando con variables y cadenas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+const string MENSAJE_NOMBRE = "¿Cómo te llamas?";
+const string MENSAJE_APELLIDO = "¿Cómo te apellidas?";
+
+string nombre;
+string apellido = "Pareja";
+
+Console.WriteLine(MENSAJE_NOMBRE);
+nombre = Console.ReadLine();
+
+Console.WriteLine(MENSAJE_APELLIDO);
+apellido = Console.ReadLine();
+
+string salida = "Hola " + nombre + " " + apellido;
+Console.WriteLine(salida);
