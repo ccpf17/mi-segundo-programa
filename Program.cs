bcfa5664d849cdf6b582d9b3e45b0ba76b7de789
ccpf17 <ccpf17@educastillalamancha.es>
2024-06-12 12:05:40 +0200

    Añadido un condicional

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ const string MENSAJE_NOMBRE = "¿Cómo te llamas?";
 const string MENSAJE_APELLIDO = "¿Cómo te apellidas?";
 
 string nombre;
-string apellido = "Pareja";
+string apellido;
 
 Console.WriteLine(MENSAJE_NOMBRE);
 nombre = Console.ReadLine();
@@ -13,3 +13,16 @@ apellido = Console.ReadLine();
 
 string salida = "Hola " + nombre + " " + apellido;
 Console.WriteLine(salida);
+
+string respuesta;
+Console.Write(nombre + " ¿Quieres que pite? (S/N)");
+respuesta = Console.ReadKey().KeyChar.ToString();
+if (respuesta.ToLower() == "s") 
+{
+    Console.WriteLine(nombre + " Has elegido pitar!");
+    Console.Beep(100, 300);
+}
+else
+{
+    Console.WriteLine(nombre + " ¡Que pena! No quieres que pite");
+}
